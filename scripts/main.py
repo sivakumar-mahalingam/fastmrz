3f9f12e7416f17cea2ed94c9399073c6b5e7def3
Sivakumar Mahalingam <sivasai2006@gmail.com>
2024-04-07 12:00:31 +0400

    updated main

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from fastmrz import FastMRZ
 import os
 
-fast_mrz = FastMRZ(tesseract_path=r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe')
+fast_mrz = FastMRZ()
 passport_mrz = fast_mrz.read_mrz(os.path.abspath('../data/passport_uk.jpg'))
 print(passport_mrz)
+
