d4eef490344307edb612fce1cb0d2dbce9495649
Sivakumar Mahalingam <sivasai2006@gmail.com>
2024-04-08 16:35:12 +0400

    updated repo

--- fastmrz/main.py
+++ fastmrz/main.py
@@ -4,7 +4,7 @@ import os
 # fast_mrz = FastMRZ(tesseract_path=r'/opt/homebrew/Cellar/tesseract/5.3.4_1/bin/tesseract')
 # fast_mrz = FastMRZ(tesseract_path=r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe')
 
-fast_mrz = FastMRZ(tesseract_path=r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe')
+fast_mrz = FastMRZ()
 passport_mrz = fast_mrz.get_mrz(os.path.abspath('../data/passport_uk.jpg'))
 print(passport_mrz)
 
