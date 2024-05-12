02a06055b23b978229c937ee682a0eb2d768b042
Sivakumar Mahalingam <sivasai2006@gmail.com>
2024-05-12 12:19:02 +0400

    updated main.py

--- fastmrz/main.py
+++ fastmrz/main.py
@@ -4,8 +4,7 @@ import json
 
 fast_mrz = FastMRZ()
 # Pass file path of installed Tesseract OCR, incase if not added to PATH variable
-# fast_mrz = FastMRZ(tesseract_path=r'/opt/homebrew/Cellar/tesseract/5.3.4_1/bin/tesseract') # Default path in Mac
-# fast_mrz = FastMRZ(tesseract_path=r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe') # Default path in Windows
+# fast_mrz = FastMRZ(tesseract_path=r'/path/to/tesseract/source')
 passport_mrz = fast_mrz.get_mrz(os.path.abspath('../data/passport_uk.jpg'))
 print("JSON:")
 print(json.dumps(passport_mrz, indent=4))
@@ -15,4 +14,3 @@ print("\n")
 passport_mrz = fast_mrz.get_raw_mrz(os.path.abspath('../data/passport_uk.jpg'))
 print("TEXT:")
 print(passport_mrz)
-
